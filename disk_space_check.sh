1ab8547fdb97d7664fb62ef6669fe6015c91a14e
Md Rayhan <rayhunkhan27@gmail.com>
2024-04-04 22:22:04 +0600

    add mail setup

--- disk_space_check.sh
+++ disk_space_check.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+# Mail
+TO="rayhunkhan27@gmail.com"
+
 # My Boot memory space
 FU=$(df -h | egrep -v "Filesystem|tmpfs" | grep "nvme0n1p1" | awk '{print $5}' | tr -d %)
 
 # Minimum Space
-MN=20
+MN=1
 
 
 if [[ $FU -gt $MN ]]
 then
-echo "Warning: Low"
+
+# Need to configure Mail or SMTP server
+echo "Warning: Low" | sendmail ssmtp $TO 
 else
 echo "Fine"
 
